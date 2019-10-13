37d34efa55394350010b66b896e7c27ecb4da42e
doglol99 <40714465+doglol99@users.noreply.github.com>
2019-10-13 17:06:56 +0100

    started work on neighbour counter and added function to print grid

--- __main__.py
+++ __main__.py
@@ -1,2 +1,35 @@
+import time
+grid=[["1"]*10]*10
+
+def main():
+    grid[4][2]
+    gridprinter(grid)
+    while True:
+        time.sleep(10)
+        for row in range(len(grid)):
+            for item in range(len(grid[row])):
+                countneighbours(grid,row,item)
+
+
+
+def countneighbours(grid,row,item):
+    count=0
+    for i in range(-1,2):
+        for j in range(-1,2):
+            if not(j==0 and i==0):
+                if grid[row][item]==1:
+                    count+=1
+                    print(count)
+    return(count)
+
+
+
+def gridprinter(grid):
+    for row in grid:
+        for item in row:
+            print(item,end="")
+        print()
+
+
 if __name__ == '__main__':
-    print("Hello, world!")
+    main()
