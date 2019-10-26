253b23ea06efa6d79584b4a13800ef6045f35db2
doglol99 <40714465+doglol99@users.noreply.github.com>
2019-10-26 13:50:38 +0100

    can now simulate conways game of life

--- __main__.py
+++ __main__.py
@@ -1,30 +1,57 @@
 import time
-grid = [[0]*10 for _ in range(10)]
 
 def main():
-    grid[4][2]=1
-    grid[3][3]=1
-    grid[2][2]=1
+    grid = [[0] * 10 for _ in range(10)]
+    grid[1][2] = 1
+    grid[2][3] = 1
+    grid[3][3] = 1
+    grid[3][2] = 1
+    grid[3][1] = 1
     print("-" * 50)
     gridprinter(grid)
-    time.sleep(1)
+    while True:
+        time.sleep(1)
+        neighbourgrid=countallneighbours(grid)
+        grid=updateallitems(grid,neighbourgrid)
+        gridprinter(grid)
+
+def updateallitems(grid,neighbourgrid):
     for row in range(len(grid)):
         for item in range(len(grid[row])):
-            print(countneighbours(grid,row,item),end="")
-        print()
+            currentitem=grid[row][item]
+            currentneighbour=neighbourgrid[row][item]
+            if currentitem>=1:
+                if currentneighbour!=3:
+                    if currentneighbour!=4:
+                        grid[row][item] = 0
+            else:
+                if currentneighbour==3:
+                    grid[row][item]=1
+    return grid
 
+
+
+
+def countallneighbours(grid):
+    y=[]
+    for row in range(len(grid)):
+        x=[]
+        for item in range(len(grid[row])):
+            x.append(countneighbours(grid, row, item))
+        y.append(x)
+    return y
 
 
 def countneighbours(grid,row,item):
     count=0
     for i in range(-1,2):
         for j in range(-1,2):
-            if not(j==0 and i==0):
-                try:
-                    if grid[row+i][item+j]==1:
-                        count+=1
-                except IndexError:
-                    True
+
+            try:
+                if grid[row+i][item+j]>=1:
+                    count+=1
+            except IndexError:
+                True
     return(count)
 
 
